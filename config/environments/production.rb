0da1b5b19b6e16469a04827309f3fa3b9a982226
Jared Swenson <jswen15@gmail.com>
2017-01-25 15:14:33 -0700

    moved exit button

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_assets = true
-  config.assets.compile = true
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -25,6 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
